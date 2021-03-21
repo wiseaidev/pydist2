1ae03216f7f1434b8b567ad3804fff17b09ba97a
Harmouch101 <mahmoudddharmouchhh@gmail.com>
2021-03-21 23:16:23 +0200

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Harmouch101/pydist2',
-    version='0.0.4',
+    version='0.0.5',
     zip_safe=False,
 )
